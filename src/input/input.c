bff2eb6011302d56873651c64e8f4bcc7dbf7230
David Fries <David@Fries.net>
2012-12-20 22:42:54 -0600

    use the header for cpu setting routines and macros

--- src/input/input.c
+++ src/input/input.c
@@ -12,6 +12,7 @@ Copyright 2012 Andrew Sampson.  Released under the GPL v2.  See COPYING.
 #include <stdint.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include "../../include/ATtiny2313_clock.h"
 
 #define LED_A_WRITE_LATCH PD2
 #define LED_B_WRITE_LATCH PD3
@@ -87,50 +88,6 @@ uint16_t readSwitches( void )
 	// the switches are low when pressed; the values are inverted here 
 	// so that the rest of the program can treat 1=pressed.
 	return ~result;
-}
-
-/* Setting the prescale speed requires two writes, _BV(CLKPCE) enables change,
- * then the new value must be written within 4 clock cycles, disable
- * interrupts to be sure.
- */
-#define CPU_PRESCALE(n)	\
-{uint8_t __read_hz=n; CLKPR = _BV(CLKPCE); CLKPR = __read_hz;}
-#define CPU_8MHz        0x00
-#define CPU_4MHz        0x01
-#define CPU_2MHz        0x02
-#define CPU_1MHz        0x03
-#define CPU_500kHz      0x04
-#define CPU_250kHz      0x05
-#define CPU_125kHz      0x06
-#define CPU_62500Hz     0x07
-#define CPU_31250Hz     0x08
-
-/* Leave this undefined to catch non-constant (invalid) input values.
- */
-uint8_t hz_is_not_valid(void);
-/* Takes the number of CPU cycles per second and returns the CPU prescaler
- * CLKPR value.  The hardware only supports 9 clock rates.
- * returns the prescale CLKPR value
- *
- * The compile will optimize the entire call to a constant.
- * Note, if the argument isn't a compiler constant, the compiler will generate
- * the function in assembly and as long as hz_is_not_valid isn't found
- * it will fail to link.  As long as it is a compiler constant value it won't
- * try to call hz_is_not_valid and work.
- */
-uint8_t cpu_hz_to_prescale(uint32_t hz);
-inline uint8_t inline_cpu_hz_to_prescale(uint32_t hz)
-{
-	return	hz==8000000 ? CPU_8MHz :
-		hz==4000000 ? CPU_4MHz :
-		hz==2000000 ? CPU_2MHz :
-		hz==1000000 ? CPU_1MHz :
-		hz==500000 ? CPU_500kHz :
-		hz==250000 ? CPU_250kHz :
-		hz==125000 ? CPU_125kHz :
-		hz==62500 ? CPU_62500Hz :
-		hz==31250 ? CPU_31250Hz :
-		hz_is_not_valid();
 }
 
 int main(void)
