48434807689e5adaac7f453f7f3b73774a6d3672
DaveHans <dhans01@earthlink.net>
2013-01-13 16:00:56 -0600

    removed wrong and added correct file

--- rocket-launch.c
+++ rocket-launch.c
@@ -3,15 +3,17 @@ This program is meant for use with an Atmel ATTiny2313, installed in a
 Hall Research KP2B keypad.  See hallResearchKP2B-reverseEngineering.txt 
 for details on the hardware.
 
-This program is an example of how to read input from the switches.
+This program is forked from keypad-input Copyright 2013 by Andrew Sampson
+  David Hans
+  Released under the GPL v2. 
 
-Copyright 2012 Andrew Sampson.  Released under the GPL v2.  See COPYING.
 */
+
 
 
 #include <stdint.h>
 #include <avr/io.h>
-#include <util/delay.h>
+
 
 #define LED_A_WRITE_LATCH PD2
 #define LED_B_WRITE_LATCH PD3
@@ -19,6 +21,8 @@ Copyright 2012 Andrew Sampson.  Released under the GPL v2.  See COPYING.
 #define SW_B_READ_OUTPUTENABLE PD5
 
 #define VALID_SWITCHES_MASK 0b1111111111
+#define VALID_LED_MASK 0b1111111111
+
 
 /*
 Uses port B to talk to the LED latches.
@@ -68,7 +72,7 @@ uint16_t readSwitches( void )
 	// I've found that a delay is necessary in order for the switch values 
 	// to appear on the bus.  This delay could possibly be shortened to 
 	// just a few nops.
-	_delay_ms( 1 );
+	asm volatile("nop");
 	// grab the values from port B
 	result |= PINB;
 	// disable bus output for the switches
@@ -79,7 +83,7 @@ uint16_t readSwitches( void )
 	// I've found that a delay is necessary in order for the switch values 
 	// to appear on the bus.  This delay could possibly be shortened to 
 	// just a few nops.
-	_delay_ms( 1 );
+	asm volatile("nop");
 	// grab the values from port B
 	result |= (PINB << 8);
 	// disable bus output for the switches
@@ -108,127 +112,98 @@ int main(void)
 	PORTD |= (1 << SW_A_READ_OUTPUTENABLE);
 	PORTD |= (1 << SW_B_READ_OUTPUTENABLE);
 	
-	/*
-	// BTW, as a curious side-effect of how the bus is wired, you can give 
-	// control of the bus to the switch latch.  If you do this, and 
-	// latch-enable one of the LED latches, the switch latch will write 
-	// directly to the LED latch.  Pressing the switches will light up the LEDs
-	// with no intervention required on the part of the microcontroller.  
-	// Here's how to do that:
-	PORTD |= (1 << LED_A_WRITE_LATCH);
-	PORTD &= ~(1 << SW_A_READ_OUTPUTENABLE);
-	while( 1 ) {}
-	*/
-
-	/*
-	// led chaser sequence
-	uint8_t i;
-	for( i = 0 ; i < 2; i++ )
-	{
-		uint16_t leds = 1;
-		// stop after all 10 leds have been illuminated
-		while( leds < (1<<11) )
-		{
-			writeLEDs( leds );
-
-			_delay_ms( 1000 );
-
-			leds = leds << 1;
-		}
-	}
-
-	// blink all leds 6 times
-	writeLEDs( 0x0000 );
-	_delay_ms( 1000 );
-	writeLEDs( 0xffff );
-	_delay_ms( 1000 );
-	writeLEDs( 0x0000 );
-	_delay_ms( 1000 );
-	writeLEDs( 0xffff );
-	_delay_ms( 1000 );
-	writeLEDs( 0x0000 );
-	_delay_ms( 1000 );
-	writeLEDs( 0xffff );
-	_delay_ms( 1000 );
-	writeLEDs( 0x0000 );
-	_delay_ms( 1000 );
-	writeLEDs( 0xffff );
-	_delay_ms( 1000 );
-	writeLEDs( 0x0000 );
-	_delay_ms( 1000 );
-	writeLEDs( 0xffff );
-	_delay_ms( 1000 );
-	writeLEDs( 0x0000 );
-	_delay_ms( 1000 );
-	writeLEDs( 0xffff );
-	_delay_ms( 1000 );
-	writeLEDs( 0x0000 );
-	_delay_ms( 1000 );
-	writeLEDs( 0xffff );
-	_delay_ms( 1000 );
-	writeLEDs( 0x0000 );
-	_delay_ms( 1000 );
-	writeLEDs( 0xffff );
-	_delay_ms( 1000 );
-	writeLEDs( 0x0000 );
-	
-	*/
 	
 	uint16_t switches = 0;
-	// read switches and update leds (momentary style), 
-	// until user presses buttons 1 and 5
-	while( (switches & VALID_SWITCHES_MASK) != 0b0000000001 )
-	{
-		switches = readSwitches();
-		writeLEDs( switches );
-	}
-	while( (switches & VALID_SWITCHES_MASK) != 0b0000000010 )
-	{
-		switches = readSwitches();
-		writeLEDs( switches );
-	}
-	while( (switches & VALID_SWITCHES_MASK) != 0b0000000100 )
-	{
-		switches = readSwitches();
-		writeLEDs( switches );
-	}
-	while( (switches & VALID_SWITCHES_MASK) != 0b0000001000 )
-	{
-		switches = readSwitches();
-		writeLEDs( switches );
-	}
+	uint16_t leds = 0;
+	uint16_t timer = 0;
+	uint8_t count = 10;
+	uint16_t armed_blink_rate = 2000;
+	uint16_t one_sec_rate = 20000;  // Just a guess
+	uint16_t ten_button = 0b1000000000;
 	
-// blink all leds 6 times
-	writeLEDs( 0x0000 );
-	_delay_ms( 500 );
-	writeLEDs( 0xffff );
-	_delay_ms( 500 );
-	writeLEDs( 0x0000 );
+	// read switches and update leds (momentary style), 
+	// until user enters the password 1 2 3 4 
+	
+while( 1 )
+	{
+//  very crude way to do this for now
+
+		while( (switches & VALID_SWITCHES_MASK) != 0b0000000001 )
+		{
+			switches = readSwitches();
+			writeLEDs( switches );
+		}
+		while( (switches & VALID_SWITCHES_MASK) != 0b0000000010 )
+		{
+			switches = readSwitches();
+			writeLEDs( switches );
+		}
+		while( (switches & VALID_SWITCHES_MASK) != 0b0000000100 )
+		{
+			switches = readSwitches();
+			writeLEDs( switches );
+		}
+		while( (switches & VALID_SWITCHES_MASK) != 0b0000001000 )
+		{
+			switches = readSwitches();
+			writeLEDs( switches );
+		}
+		
+		
+	// blink all leds until switch 10 is pressed
+	// This the armed state waiting for countdown to start	
+
+		switches = readSwitches();
+		timer = 0;
+		leds = 0;
+		writeLEDs( leds );
 
 		
-	
-	/*
-	uint16_t switchDebounce;
-	uint16_t leds = 0;
-	// read switches and toggle leds
-	while( 1 )
-	{
-		// Simple debounce: read the switches, pause, and read them again.
-		// Only count the buttons that were pressed at both samplings as 
-		// being pressed.
-		switchDebounce = readSwitches();
-		_delay_ms( 10 );
-		switchDebounce &= readSwitches();
+		while ((switches & VALID_SWITCHES_MASK) != ten_button)
+		{
+			switches = readSwitches();
+			timer++;
+			
+			if (timer >= armed_blink_rate)
+			{
+				timer = 0;
+				leds = ~ leds;
+				writeLEDs( leds );
+			}
+		}
+			
+// turn off lights and start countdown  
+// 1 button cancels and starts password entry again
+// start count at 0 then invert when we turn on lights
+
+		count = 0;
+		timer = 0;
+		leds= 0;
+		writeLEDs ( leds );
+		leds = ten_button;
 		
-		// use xor to toggle
-		// "(switches ^ switchDebounce) & switchDebounce" = 
-		// "the switches that changed and are now active"
-		leds ^= (switches ^ switchDebounce) & switchDebounce;
-		writeLEDs( leds );
+		while ((switches & VALID_SWITCHES_MASK) != 0b0000000001)
+		{
+			switches = readSwitches();
+			timer++;
 		
-		switches = switchDebounce;
+			if (timer >= one_sec_rate)
+			{
+				timer = 0;
+				count++;
+				writeLEDs( leds );
+				leds = (leds >> 1) ^ ten_button;
+				
+				if (count >= 10)
+				{ 
+///  launch here ???
+// for now just start over				
+				break; }
+				
+			}	
+		}
+		
+		
 	}
-	*/
-	
 	return 0;
-}
+}	
