a4ac5525008c053f2e36891eb12a587b0630b82d
David Fries <David@Fries.net>
2013-01-29 03:17:32 -0600

    clock_div_t and clock_prescale_set to set the clock speed

---
+++ src/keypadalike/include/avr/power.h
@@ -0,0 +1,51 @@
+/*
+This program allows limited support to compile and run programs written for
+the Atmel ATTiny2313 to work like they were in a Hall Research KP2B keypad,
+only compiled for a native desktop environment with a Qt GUI instead of
+hardware buttons and LEDs.
+
+    Copyright (C) Copyright 2012 David Fries.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef AVR_POWER_H
+#define AVR_POWER_H
+
+typedef enum
+{
+    clock_div_1 = 0,
+    clock_div_2 = 1,
+    clock_div_4 = 2,
+    clock_div_8 = 3,
+    clock_div_16 = 4,
+    clock_div_32 = 5,
+    clock_div_64 = 6,
+    clock_div_128 = 7,
+    clock_div_256 = 8,
+#if defined(__AVR_ATmega128RFA1__)
+    clock_div_1_rc = 15,
+#endif
+} clock_div_t;
+
+inline void clock_prescale_set(clock_div_t __x)
+{
+	uint8_t state=SREG;
+	cli();
+	CLKPR = _BV(CLKPCE);
+	CLKPR = __x;
+	SREG=state;
+}
+
+#endif // AVR_POWER_H
